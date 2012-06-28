4679c4a97240c6bba07a1e76c17bdd1998d7e903
Brian Pattison <brian@brianpattison.com>
2012-06-28 11:55:48 -0500

    Clean up

--- lib/motion-gravatar/motion-gravatar.rb
+++ lib/motion-gravatar/motion-gravatar.rb
@@ -3,11 +3,10 @@ class Gravatar
   
   # Generate the URL for a user's Gravatar image by providing an email address, image size, and optional default image URL.
   # @param [String] email
-  # @param [Integer] size Between 1 and 512. The default is 80. (optional)
+  # @param [Integer] size Between 1 and 512.
   # @param [String] defaultURL (optional)
   # @return [NSURL]
-  def self.getURL(email, size=nil, defaultURL=nil)
-    size = 80 if size.nil?
+  def self.getURL(email, size=80, defaultURL=nil)
     GravatarHelper.getGravatarURL(email, size:size, defaultURL:defaultURL)
   end
   
